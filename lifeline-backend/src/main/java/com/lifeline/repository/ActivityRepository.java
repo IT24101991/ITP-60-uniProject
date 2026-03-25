8b918ca8c0434276d69872cdf85224aeb6e9b2dc
IT24100304 <it24100304@my.sliit.lk>
2026-03-25 21:39:14 +0530

    Delete lifeline-backend directory

--- lifeline-backend/src/main/java/com/lifeline/repository/ActivityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lifeline.repository;
-
-import com.lifeline.model.ActivityLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ActivityRepository extends JpaRepository<ActivityLog, Long> {
-    List<ActivityLog> findTop10ByOrderByTimestampDesc();
-}
